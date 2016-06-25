f79544e92ef3467eefb3cd45534cceede31d7a4c
Phas3r <sergeisl95@yandex.ru>
2016-06-25 23:57:07 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ var db = new sqlite.Database('db.sqlite');
 
 
 app.get('/', function (req, res) {
-    db.all('select * from sites', function(err, result){
+    db.all('select * from sites', function (err, result) {
         res.render('index.ejs', {items: result});
     })
         
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
