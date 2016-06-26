b74033d88ce982584ce25472787c8725a89bd348
Phas3r <sergeisl95@yandex.ru>
2016-06-26 19:16:04 +0300

    added authentication

--- app.js
+++ app.js
@@ -1,10 +1,77 @@
 var express = require('express');
 var sqlite = require('sqlite3');
 var app = express();
-
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
 var db = new sqlite.Database('db.sqlite');
+var passport = require('passport');
+var ls = require('passport-local').Strategy;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use(cookieParser());
+app.use(require('express-session')({ secret: 'hehkek', resave: false, saveUninitialized: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+function makeHash(psswd, salt) {
+    var hash = crypto.createHash('sha256');
+    hash.update(psswd);
+    hash.update(salt);
+    return hash.digest('hex');
+}
+
+
+
+passport.use(new ls(
+    {
+        usernameField: 'login',
+        passwordField: 'password'
+    },
+    function(login, password, done) {
+    db.get('SELECT id, name FROM Users WHERE name = ? AND password = ?', login, password, function(err, row) {
+        if (!row) return done(null, false);        
+        return done(null, row);
+    });
+}));
+passport.serializeUser(function(user, done) {
+    return done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+    db.get('SELECT id, name FROM Users WHERE id = ?', id, function(err, row) {
+        if (!row) return done(null, false);
+        return done(null, row);
+    });
+});
+
+app.get('/login', function(request, response){
+    if (request.isAuthenticated()) {
+        response.redirect('/');
+    }
+    response.render('login.ejs');
+});
+app.post('/login', passport.authenticate('local',
+    {session: true, successRedirect: '/', failureRedirect: '/login' }
+));
+
+app.all('/*', function(request, response, next) {
+    if (!request.isAuthenticated()) {
+        response.redirect('/login');
+    }
+    next();  // call next() here to move on to next middleware/router
+});
+
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+
 
 
 app.get('/', function (req, res) {
@@ -14,9 +81,6 @@ app.get('/', function (req, res) {
         
 });
 
-app.get('/', function (req, res) {
-        res.render('index.ejs');
-   });
 
 app.post('/add', function(req, res){
     var statement = 'insert into sites (Name, Url, Description) values(?, ?, ?)';
@@ -31,6 +95,15 @@ app.delete('/delete', function (req, res) {
         res.send('deleted');
     });
 });
+
+
+app.get('/grid', function (req, res) {
+        res.render('grid.ejs');
+   });
+
+
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
